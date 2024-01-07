a3a98ae4560e006ed58463d93718791a5704f821
Akash02032002 <aksahkumar02032002@gmail.com>
2024-01-07 20:02:11 +0530

    new one update in url

--- app.js
+++ app.js
@@ -8,35 +8,37 @@ app.use(express.json());
 app.use(cors());
 
 // checkout api
-app.post("/api/create-checkout-session",async(req,res)=>{
-    const {products} = req.body;
 
+app.post("/api/create-checkout-session", async (req, res) => {
+  const { products } = req.body;
 
-    const lineItems = products.map((product)=>({
-        price_data:{
-            currency:"inr",
-            product_data:{
-                name:product.dish,
-                images:[product.imgdata]
-            },
-            unit_amount:product.price * 100,
-        },
-        quantity:product.qnty
-    }));
+  const lineItems = products.map((product) => ({
+    price_data: {
+      currency: "inr",
+      product_data: {
+        name: product.dish,
+        images: [product.imgdata],
+      },
+      unit_amount: product.price * 100,
+    },
+    quantity: product.qnty,
+  }));
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types:["card"],
-        line_items:lineItems,
-        mode:"payment",
-        success_url:"https://food-court-one.vercel.app/sucess",
-        cancel_url:"https://food-court-one.vercel.app/cancel",
-    });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "https://food-court-one.vercel.app/success",
+    cancel_url: "https://food-court-one.vercel.app/cancel",
+  });
 
-    res.json({id:session.id})
- 
-})
+  res.json({ id: session.id });
+});
 
+const PORT = process.env.PORT || 7000;
 
-app.listen(7000,()=>{
-    console.log("server start")
-})
+app.listen(PORT, () => {
+  console.log(
+    `Server started at https://foodcourt-server.onrender.com:${PORT}`
+  );
+});
